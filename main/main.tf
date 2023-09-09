0cde74074b7c21eb2c4a4508d1207c52c591e2a0
fmtacuri <fmtacuri@uce.edu.ec>
2023-09-09 13:13:18 -0500

    Init

--- main/main.tf
+++ main/main.tf
@@ -17,35 +17,35 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-## Create a bucket AWS S3
-#resource "aws_s3_bucket" "s3_example" {
-#  bucket = "bucket-flugel-1234567654"
-#  tags   = {
-#    Name        = "Flugel"
-#    Environment = "Dev"
-#    Owner       = "InfraTeam"
-#  }
-#}
-#
-##  Create a AWS EC2 Instance
-#resource "aws_instance" "ec2_example" {
-#  ami           = "ami-830c94e3"
-#  instance_type = "t2.micro"
-#  tags = {
-#    Name        = "Flugel"
-#    Environment = "Dev"
-#    Owner       = "InfraTeam"
-#  }
-#}
-#
-## variables for automation testing
-#output "bucket_id" {
-#  value = trimspace(aws_s3_bucket.s3_example.id)
-#}
-#
-#output "instance_id" {
-#  value = trimspace(aws_instance.ec2_example.tags.Name)
-#}
+# Create a bucket AWS S3
+resource "aws_s3_bucket" "s3_example" {
+  bucket = "bucket-flugel-1234567654"
+  tags = {
+    Name        = "Flugel"
+    Environment = "Dev"
+    Owner       = "InfraTeam"
+  }
+}
+
+#  Create a AWS EC2 Instance
+resource "aws_instance" "ec2_example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "Flugel"
+    Environment = "Dev"
+    Owner       = "InfraTeam"
+  }
+}
+
+# variables for automation testing
+output "bucket_id" {
+  value = trimspace(aws_s3_bucket.s3_example.id)
+}
+
+output "instance_id" {
+  value = trimspace(aws_instance.ec2_example.tags.Name)
+}
 
 
 /////////////////////////////////////
